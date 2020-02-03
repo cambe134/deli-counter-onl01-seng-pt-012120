7e33fcf16c582a4348754077f9674e0aebbb8c1c
Spencer Cambern <cambern134@gmail.com>
2020-02-03 22:01:57 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,11 +1,11 @@
 def line(array)
   if array.length >= 1
     nuarray = []
-    counter = 1 
+    counter = 1
     array.each do |name|
       nuarray.push("#{counter}. #{name}")
-      counter += 1 
-    end 
+      counter += 1
+    end
     puts "The line is currently: #{nuarray.join(" ")}"
   else
     puts "The line is currently empty."
@@ -14,10 +14,10 @@ end
 
 line(katz_deli)
 
-def line_simple(array) 
+def line_simple(array)
   current_line = "The simple line is currently:"
-  array.each.with_index(1) do |value, indexemus|  
-    current_line << " #{indexemus}. #{value},"    
-  end 
+  array.each.with_index(1) do |value, indexemus|
+    current_line << " #{indexemus}. #{value},"
+  end
   puts current_line
 end 
