28f9da7fd11742bc75402d6215c483fa4dcde451
Spencer Cambern <cambern134@gmail.com>
2020-02-03 22:05:12 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,14 +1,14 @@
 katz_deli = ["Moshe", "Fayge", "Rivki"]
 
 
-def line(array) 
+def line(array)
   if array.length >= 1
     nuarray = []
-    counter = 1 
+    counter = 1
     array.each do |name|
       nuarray.push("#{counter}. #{name}")
-      counter += 1 
-    end 
+      counter += 1
+    end
     puts "The line is currently: #{nuarray.join(" ")}"
   else
     puts "The line is currently empty."
@@ -17,22 +17,22 @@ end
 
 line(katz_deli)
 
-def line_simple(array) 
+def line_simple(array)
   current_line = "The simple line is currently:"
-  array.each.with_index(1) do |value, indexemus|  
+  array.each.with_index(1) do |value, indexemus|
 
-    current_line << " #{indexemus}. #{value},"    
-  
-  end 
+    current_line << " #{indexemus}. #{value},"
+
+  end
   puts current_line
-end 
-  
-line_simple(katz_deli)  
+end
+
+line_simple(katz_deli)
 
 
 
 def take_a_number(line, new_person)
-  line.push(new_person) 
+  line.push(new_person)
   puts "Welcome, #{new_person}. You are number #{line.length} in line."
 end
 
@@ -42,7 +42,7 @@ def now_serving(line)
   if line.length == 0 "
     puts"There is nobody waiting to be served!"
   else
-    puts "Currently serving 
+    puts "Currently serving
   end
 end
 
