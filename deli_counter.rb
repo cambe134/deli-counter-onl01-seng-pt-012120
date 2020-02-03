f7a286c9c6c86cee64f5f776060b32290b7051d1
Spencer Cambern <cambern134@gmail.com>
2020-02-03 22:03:36 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -23,22 +23,3 @@ def line_simple(array)
 end
 
 line_simple(katz_deli)
-
-def take_a_number(line, new_person)
-  line.push(new_person)
-  puts "Welcome, #{new_person}. You are number #{line.length} in line."
-end
-
-take_a_number(katz_deli, "Fyvish")
-
-def now_serving(line)
-  if line.length == 0
-    puts"There is nobody waiting to be served!"
-  else
-    puts "Currently serving #{line[0]}."
-    line.shift
-  end
-end
-
-puts now_serving(katz_deli)
-puts katz_deli
